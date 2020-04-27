132085915cdd8a4ea99c1eb0882ae25f1fc48ff5
victusfate <messel@gmail.com>
2020-04-27 16:25:03 -0400

    AR-171 correct download script

--- photo_3d/download.sh
+++ photo_3d/download.sh
@@ -7,4 +7,5 @@ mv depth-model.pth checkpoints/.
 wget https://s3.amazonaws.com/dev.welco.me/3d/model/edge-model.pth
 mv edge-model.pth checkpoints/.
 wget https://s3.amazonaws.com/dev.welco.me/3d/model/model.pt
+mkdir MidaS
 mv model.pt MiDaS/.
