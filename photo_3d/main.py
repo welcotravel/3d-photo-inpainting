9f58ce3ebce964337797675cc1405e16e33f53fe
victusfate <messel@gmail.com>
2020-04-27 14:09:42 -0400

    AR-170 AR-171 local testing

--- photo_3d/main.py
+++ photo_3d/main.py
@@ -75,7 +75,7 @@ def create_3d_video(input_path=None,x_shift_range=[],y_shift_range=[],z_shift_ra
   sample_list = get_MiDaS_samples(config['src_folder'], config['depth_folder'], config, config['specific'])
   normal_canvas, all_canvas = None, None
 
-  if isinstance(config["gpu_ids"], int) and (config["gpu_ids"] >= 0):
+  if torch.cuda.is_available() and isinstance(config["gpu_ids"], int) and (config["gpu_ids"] >= 0):
       device = config["gpu_ids"]
   else:
       device = "cpu"
