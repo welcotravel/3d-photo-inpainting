e268877693feb214d5da06bbd62bf5b002f13e1d
victusfate <messel@gmail.com>
2020-05-04 11:47:10 -0400

    AR-200 merge is working locally, now to test cython optimizations on dev

--- test/process_image.py
+++ test/process_image.py
@@ -8,13 +8,13 @@ temp_dir = tempfile.mkdtemp()
 os.system('cp ../photo_3d/image/' + image_base + ' ' + os.path.join(temp_dir,image_base))
 
 video_files = image_processor.create_3d_video(
-  fps=40,
-  num_frames=160,
+  fps=30,
+  num_frames=180,
   input_path=temp_dir,
   output_path='./',
-  x_shift_range=[-0.02],
+  x_shift_range=[0.00],
   y_shift_range=[0.00],
-  z_shift_range=[-0.07],
+  z_shift_range=[-0.05],
   traj_types=['double-straight-line'],
   video_postfix=['dolly-zoom-in']
 )
